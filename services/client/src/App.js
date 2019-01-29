e18cd77ad5bc89439c92934d57bd9b17a73e34f4
maximeroma <m.roma@techexmachina.com>
2019-01-29 19:49:58 +0100

    add users

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { getUsers } from "./services/users";
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { getUsers } from "./services/users"
 
 class App extends Component {
-  static defaultProps = { getUsers };
+  static defaultProps = { getUsers }
+
+  state = { users: [] }
 
   componentDidMount() {
-    getUsers();
+    getUsers()
+      .then(users => this.setState({ users: users.data.data.users }))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -18,14 +22,21 @@ class App extends Component {
             <div className="column is-one-third">
               <br />
               <div className="title is-1 is-1">All Users</div>
+              {this.state.users.map(user => {
+                return (
+                  <h4 key={user.id} className="box title is-4">
+                    {user.username}
+                  </h4>
+                )
+              })}
               <hr />
               <br />
             </div>
           </div>
         </div>
       </section>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- services/client/src/services/users.js
+++ services/client/src/services/users.js
@@ -2,8 +2,4 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_USERS_SERVICE_URL;
 
-export const getUsers = () =>
-  axios
-    .get(`${url}/users`)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+export const getUsers = () => axios.get(`${url}/users`);
