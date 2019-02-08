2ede8e75320dcc588b0ef6e5344236bed6575163
maximeroma <m.roma@techexmachina.com>
2019-02-08 21:57:07 +0100

    add decode and encode method for auth token

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -16,7 +16,9 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY')
+        )
         self.assertFalse(current_app is None)
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI']
                         == os.environ.get('DATABASE_URL'))
@@ -33,7 +35,9 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY')
+        )
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI']
@@ -51,12 +55,15 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY')
+        )
         self.assertFalse(app.config['TESTING'])
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
         self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
         self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
+
 
 if __name__ == '__main__':
     unittest.main()
