47fd63f8506a10cc29ba82e47baa93555a15a3aa
maximeroma <m.roma@techexmachina.com>
2019-03-03 14:08:44 +0100

    reset to previous commit

--- services/client/src/pages/home.js
+++ services/client/src/pages/home.js
@@ -1,50 +1,36 @@
-import React, {Component} from "react"
-import {getUsers, addUser} from "services/users"
+import React, {useEffect} from "react"
 import AddUser from "components/add-user"
 import UsersList from "components/users-list"
+import useApi from "hooks/use-api"
 
-class Home extends Component {
-  static defaultProps = {getUsers, addUser}
+export function Home() {
+  const {doGet, doPost, postUsers, getUsers} = useApi()
 
-  state = {users: []}
+  useEffect(() => {
+    doGet("/users")
+  }, [postUsers])
 
-  componentDidMount() {
-    this.getUsers()
-  }
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-one-third">
+            <br />
+            {getUsers && getUsers.isLoading && <div>...loading</div>}
+            {getUsers && getUsers.isError && <div>...error</div>}
+            <AddUser addUser={values => doPost("/users", values)} />
+            <br />
+            {getUsers && getUsers.data && (
+              <UsersList users={getUsers.data.users} />
+            )}
+            <hr />
 
-  getUsers = () => {
-    const {getUsers} = this.props
-    getUsers()
-      .then(users => this.setState({users: users.data.data.users}))
-      .catch(err => console.log(err))
-  }
-
-  addUser = ({username = "", email = ""}) => {
-    return addUser({username, email})
-      .then(res => {
-        this.getUsers()
-      })
-      .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br />
-              <AddUser addUser={this.addUser} />
-              <br />
-              <UsersList users={this.state.users} />
-              <hr />
-              <br />
-            </div>
+            <br />
           </div>
         </div>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
 
 export default Home
