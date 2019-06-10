56668e9d62b6a73365c381cff15f16f7830eac4c
maximeroma <m.roma@techexmachina.com>
2019-06-10 19:09:46 +0200

    setup apollo client

--- services/client/src/AppProvider.js
+++ services/client/src/AppProvider.js
@@ -1,4 +1,13 @@
 import React from "react"
 import {AuthProvider} from "hooks/auth"
+import {client} from "graphql/setup"
+import {ApolloProvider} from "react-apollo"
+import {ApolloProvider as ApolloHooksProvider} from "react-apollo-hooks"
 
-export default ({children}) => <AuthProvider>{children}</AuthProvider>
+export default ({children}) => (
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <AuthProvider>{children}</AuthProvider>
+    </ApolloHooksProvider>
+  </ApolloProvider>
+)
--- services/client/src/routes/home.js
+++ services/client/src/routes/home.js
@@ -1,50 +1,50 @@
 import React, {Component} from "react"
 import {getUsers, addUser} from "services/users"
+import gql from "graphql-tag"
+import {useQuery} from "react-apollo-hooks"
 
 import UsersList from "components/users-list"
 
-class Home extends Component {
-  static defaultProps = {getUsers, addUser}
+const ALL_USERS = gql`
+  {
+    allUsers {
+      edges {
+        node {
+          id
+          email
+          username
+          active
+          admin
+        }
+      }
+    }
+  }
+`
 
-  state = {users: []}
-
-  componentDidMount() {
-    this.getUsers()
+const Home = () => {
+  const {data, loading, error} = useQuery(ALL_USERS)
+  if (error) {
+    return <div>Error</div>
   }
 
-  getUsers = () => {
-    const {getUsers} = this.props
-    getUsers()
-      .then(users => {
-        this.setState({users: users.data.data.users})
-      })
-      .catch(err => console.log(err))
+  if (loading) {
+    return <div>Loading</div>
   }
 
-  addUser = ({username = "", email = ""}) => {
-    return addUser({username, email})
-      .then(res => {
-        this.getUsers()
-      })
-      .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br />
-              <UsersList users={this.state.users} />
-              <hr />
-              <br />
-            </div>
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-one-third">
+            <br />
+            <UsersList users={data.allUsers.edges} />
+            <hr />
+            <br />
           </div>
         </div>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
 
 export default Home
